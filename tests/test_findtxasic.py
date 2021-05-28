d0da3c7742ce622224cf0d0bc8a2833d5986f14e
Rebecca <anzalonerebecca2@gmail.com>
2021-05-28 18:41:07 +0200

    update test_findtxasic

--- tests/test_findtxasic.py
+++ tests/test_findtxasic.py
@@ -4,18 +4,24 @@ import sys
 
 import numpy as np
 #import CMEPDA_exam
-from CMEPDA_exam.functionbox import find_tx_asic
+from functionbox import find_tx_asic
+from functionbox import T_ASIC_TEMP_EVENT
 #import CMEPDA_exam.functionbox as fun
 #from functionbox import find_tx_asic
 
-infile=np.fromfile('/home/rebecca/Scrivania/programmi/energy_tx12_asic8910.dat.dec',dtype=fun.T_ASIC_TEMP_EVENT)
+infile=np.fromfile('file.dat.dec',dtype=T_ASIC_TEMP_EVENT)
+
+with open('file_test.dat.dec','wb+') as f:
+    test=infile[:15]
+    test.tofile(f)
+
 
 found_tx,found_ic = find_tx_asic(infile)
 
 class Test_findtxasic(unittest.TestCase):
      def test_foundtxasic(self):
          self.assertEqual(found_tx,[12])
-         self.assertEqual(found_ic,[8,9,10])
+         self.assertEqual(found_ic,[2])
 
 
 
